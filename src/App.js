2d8eb4c8620b4002302df462f8d8b905ca4866e1
beverlyosoria <bevosoria@gmail.com>
2020-02-18 01:31:19 -0600

    adds jumbotron to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import {Jumbotron, Container} from 'react-bootstrap'
+
 import './App.css';
 
 class App extends Component {
 	render() {
-		return <div className="App">Hello Worlddd</div>;
+		return (
+			<div className="App">
+				<Jumbotron  fluid>
+  <Container>
+    <h1>Beverly Osoria</h1>
+    <p>
+    Hi! I'm a front end focused full stack developer. Passionate about making a
+											social impact in the ever-evolving space of technology.
+    </p>
+  </Container>
+</Jumbotron>
+			</div>
+		);
 	}
 }
 
