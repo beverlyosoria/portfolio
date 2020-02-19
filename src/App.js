a80e9feb4dbf11cc33c925f5ca6ddfe3c3bee6d7
beverlyosoria <bevosoria@gmail.com>
2020-02-19 01:48:49 -0600

    adds google font and favicon

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Jumbotron  fluid>
-  <Container>
-    <h1>Beverly Osoria</h1>
-    <p>
-    Hi! I'm a front end focused full stack developer. Passionate about making a
-											social impact in the ever-evolving space of technology.
-    </p>
-  </Container>
-</Jumbotron>
+				<Jumbotron fluid>
+        <Container>
+        <h1>Beverly Osoria</h1>
+        <p>Hi! I'm a front end focused full stack developer. Passionate about making a social impact in the ever-evolving space of technology.
+        </p>
+        </Container>
+        </Jumbotron>
 			</div>
 		);
 	}
