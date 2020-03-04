a0d175fc2a17cf6d77e6739b871d1b202d5fe766
beverlyosoria <bevosoria@gmail.com>
2020-03-04 14:48:10 -0600

    adds links to project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import deafinitely from "../../src/imgs/deafinitely.jpg";
 
@@ -18,22 +19,52 @@ class Projects extends Component {
           Projects
         </h1>
         <Container>
-          {/* Project 1 Deafinitely Friendly */}
+          {/* Project 4 Deafinitely Friendly */}
           <Row>
             <Col lg={4} sm={12}>
               <h2>Deafinitely Friendly</h2>
 
               <p>
-                a full-stack web application working with React and a third
-                party API to search for local businesses and post reviews.
-                User’s data is stored on a backend created using Node, Express,
-                and MongoDB. Includes user authentication with JWT.
+                Deafinitely Friendly allows you to search for local businesses,
+                rate and post reviews based on the criteria of
+                deaf-friendliness. Sign up, Log in to leave a review or simply
+                browse reviews left by previous users.
               </p>
 
-              <p>
-                A reconstructed version of Search It. Rate and post reviews for
-                a local business based on the criteria of deaf-friendliness.
-              </p>
+              <ul>JavaScript</ul>
+              <ul>React </ul>
+              <ul>MDB React</ul>
+              <ul> Express </ul>
+              <ul>Node.js</ul>
+              <ul>MongoDb</ul>
+              <ul>Mongoose</ul>
+              <ul>Heroku</ul>
+              {/* Live Heroku Link */}
+              <Link
+                to="https://deafinitely-friendly.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  color: "inherit",
+                  textDecoration: "none"
+                }}
+              >
+                <button id="project-button" className="learn-more">
+                  See Live
+                </button>
+              </Link>
+              {/* Github Repo Link */}
+              <Link
+                to="https://github.com/beverlyosoria/deafinitely-friendly"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  color: "inherit",
+                  textDecoration: "none"
+                }}
+              >
+                Source Code
+              </Link>
             </Col>
             <Col lg={8} sm={12}>
               <Image
