a86bd675276fadc99a955972b94d6b465b60af73
beverlyosoria <bevosoria@gmail.com>
2020-03-03 21:16:07 -0600

    adds new font and box shadow to project images

--- src/components/projects.js
+++ src/components/projects.js
@@ -20,8 +20,21 @@ class Projects extends Component {
         <Container>
           {/* Project 1 Deafinitely Friendly */}
           <Row>
-            <Col lg={4} sm={12}></Col>
+            <Col lg={4} sm={12}>
+              <h2>Deafinitely Friendly</h2>
 
+              <p>
+                a full-stack web application working with React and a third
+                party API to search for local businesses and post reviews.
+                User’s data is stored on a backend created using Node, Express,
+                and MongoDB. Includes user authentication with JWT.
+              </p>
+
+              <p>
+                A reconstructed version of Search It. Rate and post reviews for
+                a local business based on the criteria of deaf-friendliness.
+              </p>
+            </Col>
             <Col lg={8} sm={12}>
               <Image
                 src={deafinitely}
